da5503a1418610f40caaa7829d0424a4d3244b7a
Ben Curren <ben@outright.com>
2011-12-10 10:56:28 -0800

    simplified our recipes a bit

--- lib/recipes/unicorn.rb
+++ lib/recipes/unicorn.rb
@@ -1,4 +1,7 @@
 Capistrano::Configuration.instance(:must_exist).load do
+  _cset(:unicorn_config) { "#{current_path}/config/unicorn.rb" }
+  _cset(:unicorn_pid) { "#{shared_path}/pids/unicorn.pid" }
+  
   namespace :unicorn do
     desc "Setup unicorn specific directories"
     task :setup, :roles => [:app], :except => { :no_release => true } do
@@ -6,69 +9,30 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     desc "Create symlink"
-    namespace :symlink do
-      task :default do
-        matched = get_servers(self, :roles => [:app], :except => { :no_release => true})
-        if matched.size > 0
-          unicorn.symlink.on_servers
-        end
-      end
-      task :on_servers, :roles => [:app], :except => { :no_release => true } do
-        run "cd #{current_path} && ln -sf #{shared_path}/sockets tmp/sockets"
-        run "cd #{current_path} && ln -sf #{shared_path}/pids tmp/pids"
-      end
+    task :symlink, :roles => [:app], :except => { :no_release => true } do
+      run "cd #{current_path} && ln -sf #{shared_path}/sockets tmp/sockets"
+      run "cd #{current_path} && ln -sf #{shared_path}/pids tmp/pids"
     end
 
     desc "Start unicorn from scratch"
     task :start, :roles => [:app], :except => { :no_release => true } do
-      if !pid_is_running?(unicorn_pid)
-        run "cd #{current_path}; #{sudo} bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D || ( tail -100 #{current_path}/log/unicorn.errors ; exit 1 )"
-      else
-        logger.info "Unicorn already started. Doing nothing."
-      end
+      run "cd #{current_path}; #{sudo} bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D || ( tail -100 #{current_path}/log/unicorn.errors ; exit 1 )"
     end
 
     desc "Gracefully stop unicorn from serving requests"
     task :stop, :roles => [:app], :except => { :no_release => true } do
-      if pid_is_running?(unicorn_pid)
-        run "#{sudo} kill -QUIT `cat #{unicorn_pid}`; #{sudo} rm #{unicorn_pid}"
-        unicorn.tail.stop
-      else
-        logger.info "Unicorn is not running. Doing nothing."
-      end
+      run "#{sudo} kill -QUIT `cat #{unicorn_pid}`; #{sudo} rm #{unicorn_pid}"
+      unicorn.tail.stop
     end
 
-    desc "Update unicorn without dropping any connections"
-    task :upgrade, :roles => [:app], :except => { :no_release => true } do
-      sudo "kill -USR2 `cat #{unicorn_pid}`"
-    end
-
-    desc "Start or upgrade unicorn"
-    namespace :restart do
-      task :default do
-        if get_servers(self, :role => :app, :except => { :no_release => true}).size > 0
-          unicorn.restart.on_servers
-        end
-      end
-      task :on_servers, :roles => [:app], :except => { :no_release => true } do
-        if pid_is_running?(unicorn_pid)
-          unicorn.upgrade
-        else
-          unicorn.start
-        end
-      end
+    desc "Restart unicorn"
+    task :restart, :roles => [:app], :except => { :no_release => true } do
+      sudo "kill -USR2 `cat #{unicorn_pid}`", :on_no_matching_servers => :continue
     end
 
     desc "Reopen log files"
     task :reopen_logs, :roles => [:app], :except => { :no_release => true } do
       sudo "kill -USR1 `cat #{unicorn_pid}`"
-    end
-
-    namespace :confirm do
-      task :stop, :roles => [:app], :except => { :no_release => true } do
-        pid = capture("ps -ef | grep unicorn | grep -v grep | grep master | awk '{print $2 }'").chomp
-        run "while [ `ps -ef | grep unicorn | grep -v grep | grep master | awk '{ print $2 }'` -eq #{pid} ] ; do sleep 1; done ;"
-      end
     end
 
     namespace :tail do
